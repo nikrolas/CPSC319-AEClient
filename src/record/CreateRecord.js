f087e659be65f245533e4aa65fdf507e356ee6c7
Nicholas <nicholas.s.chan@gmail.com>
2018-02-06 23:58:36 -0800

    Replaced with more accurate dummy data, updated mapping, and dynamic rendering of children at level 1

--- src/record/CreateRecord.js
+++ src/record/CreateRecord.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
+import {Button, FormGroup, ControlLabel, FormControl,Checkbox} from 'react-bootstrap'
 
 class CreateRecord extends React.Component{
 
@@ -11,7 +11,35 @@ class CreateRecord extends React.Component{
                 recordNumber: "",
                 title:"",
                 location: "Burnaby",
-                recordJson:{"Subject":"KKK-yyyy/ggg","AE CORP - CEO - BOARD MINUTES":"KKK-XXXXXXXXXXXXXXXXXXXXXX"},
+                classificationChildren:[],
+                recordJson:[{
+                    "id": "10",
+                    "name": "CASE RECORDS",
+                    "numberPattern": "XXX-ZZZ/NN",
+                    "defaultScheduleId": "322"
+                }, {
+                    "id": "3",
+                    "name": "subject",
+                    "numberPattern": "KKK-yyyy/ggg",
+                    "defaultScheduleId": ""
+                }]
+                ,
+                classificationJson:[{
+                    "id": "2",
+                    "name": "COMPLIANCE",
+                    "keyword": "F",
+                    "updatedAt": "2003-10-10 19:00:48.000000",
+                    "parent": "",
+                    "children": [3,4,5]
+                }, {
+                    "id": "3",
+                    "name": "SAMPLE",
+                    "keyword": "G",
+                    "updatedAt": "2003-10-10 19:00:52.000000",
+                    "parent": 2,
+                    "children": ""
+                }]
+                ,
                 checked: false
             };
 
@@ -22,36 +50,55 @@ class CreateRecord extends React.Component{
 
     handleChange(e) {
         this.setState({[e.target.name]: e.target.value});
-        //Obtain record number from dropdown and set state recordNumber
+        //When RecordType is changed, adjust record number
         if(e.target.name === "recordType") {
             for(var k in this.state.recordJson) {
                 if(k === e.target.value) {
-                    this.setState({recordNumber:this.state.recordJson[k]});
+                    this.setState({recordNumber:this.state.recordJson[k]["numberPattern"]});
+                    break;
+                }
+            }
+        }
+        //When Classification is changed, populate clickbox with proper children
+        if(e.target.name === "classification") {
+            for( k in this.state.classificationJson) {
+                if(k === e.target.value) {
+                    this.setState({classificationChildren:this.state.classificationJson[k]["children"]});
+                    console.log(this.state.classificationChildren);
                     break;
                 }
             }
         }
     }
 
+    returnCheckboxes(){
+        if(this.state.classificationChildren.length > 0) {
+            return this.state.classificationChildren.map((item,i)=>
+                <Checkbox inline key ={i} value = {i}>{item}</Checkbox>);
+        }
+        else {
+            return null;
+        }
+    };
+
     handleSubmit(event) {
         alert('Form has been submitted');
         event.preventDefault();
     }
-    dropdownRecordType() {
-        let keys = [];
-        for (var k in this.state.recordJson) {
-            keys.push(<option key={k} value={k}>{k}</option>);
-        }
-        return keys;
-    }
 
     render() {
+        const listRecordTypeJson = this.state.recordJson.map((item,i)=><option key={i} value={i}>{item.name}</option>);
+        const listClassificationJson= this.state.classificationJson.map((item,i)=>
+            <option key={i} value={i}>{item.name}</option>);
+
         return (
             <form onSubmit={this.handleSubmit}>
                 <FormGroup controlId="formControlsSelect " onChange ={this.handleChange}>
                     <ControlLabel>Record Type*</ControlLabel>
-                    <FormControl name="recordType" componentClass="select" placeholder="select">
-                        {this.dropdownRecordType()}
+                    <FormControl name="recordType"
+                                 componentClass="select"
+                                 placeholder="select">
+                        {listRecordTypeJson}
                     </FormControl>
                 </FormGroup>
                 <FormGroup>
@@ -73,7 +120,28 @@ class CreateRecord extends React.Component{
                         placeholder="Enter text"
                         onChange={this.handleChange}
                     />
-                </FormGroup>;
+                </FormGroup>
+                <FormGroup>
+                    <ControlLabel>Title*</ControlLabel>
+                    <FormControl
+                        name="title"
+                        type="text"
+                        value={this.state.title}
+                        placeholder="Enter text"
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="formControlsSelect " onChange ={this.handleChange}>
+                    <ControlLabel>Classification</ControlLabel>
+                    <FormControl name="classification"
+                                 componentClass="select"
+                                 placeholder="select">
+                        {listClassificationJson}
+                    </FormControl>
+                </FormGroup>
+                <FormGroup>
+                    {this.returnCheckboxes()}
+                </FormGroup>
                 <Button type="submit">Submit</Button>
             </form>
         )
