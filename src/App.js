fac208217cbde9cbb2ceb5b6baab9c3b52f2645c
AndrewChang17 <andrewchang17@hotmail.com>
2018-02-07 01:07:25 +0000

    Revert "Search Result Table"
    This reverts commit 41776238d3b6addcadbcd65c4c300aded6e5921f

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import RecordSearchInput from './search/RecordSearchInput';
 import LayoutBanner from './banner/LayoutBanner'
-import SelectTable from "./search/Results";
 
 
 class App extends Component {
@@ -12,7 +11,6 @@ class App extends Component {
           <LayoutBanner/>
         <br/>
       <RecordSearchInput/>
-          <SelectTable/>
       </div>
     );
   }
