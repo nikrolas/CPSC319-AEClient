0f0c7319ca1ec8634cec27e49fc84ac304e8cfb1
Ryan Koon <ryankoon@alumni.ubc.ca>
2018-03-10 14:51:56 -0800

    Added Integration tests for homepage and search. Added fixtures for containers, records, and search results

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,27 @@
+describe('home', function () {
+    beforeEach(function () {
+        cy.visit('http://localhost:3000');
+    });
+
+    it('should have search input', function () {
+        cy.get('input')
+            .should('have.attr', 'placeholder', 'Search by record number. e.g. VAN')
+            .type('345')
+            .should('have.value', '345');
+    });
+
+    describe('nav bar', function () {
+        it('should have search', function () {
+            cy.contains('Search').click();
+            cy.url().should('include', '/results');
+        });
+        it('should have work tray', function () {
+            cy.contains('Work Tray').click();
+            cy.url().should('include', '/worktray');
+        });
+        it('should have new record', function () {
+            cy.contains('New Record').click();
+            cy.url().should('include', '/createRecord');
+        });
+    });
+});
---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,39 @@
+describe('search', function () {
+    let serviceRoot = "http://localhost:3000";
+
+    beforeEach(function () {
+        cy.visit(serviceRoot);
+    });
+
+    it('should search with enter input', function () {
+        cy.get('input')
+            .should('have.attr', 'placeholder', 'Search by record number. e.g. VAN')
+            .type('345{enter}');
+        cy.get('input').should('have.value', '345');
+    });
+
+    it('should search with click', function () {
+        cy.get('input')
+            .should('have.attr', 'placeholder', 'Search by record number. e.g. VAN')
+            .type('345')
+            .should('have.value', '345');
+        cy.get('button').click();
+        cy.get('input').should('have.value', '345');
+    });
+
+    it('should return results', function () {
+        cy.server();
+        cy.route("**", 'fixture:results.json').as('getComment');
+
+        cy.get('input')
+            .should('have.attr', 'placeholder', 'Search by record number. e.g. VAN')
+            .type('345{enter}');
+
+        cy.wait('@getComment').should(function (response) {
+            expect(response.status).to.eq(200);
+            expect(response.responseBody.length).to.be.greaterThan(0);
+            expect(response.responseBody[0].title).to.eq("Test title 1");
+        });
+    });
+
+});
