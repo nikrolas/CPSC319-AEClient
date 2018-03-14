dfb7af25f722780c8504e9d3b96544a820ca639a
Nicholas <nicholas.s.chan@gmail.com>
2018-03-13 23:08:13 -0700

    Added in number patterns validation mimic from back-end. Still needs to be implemented

--- src/APIs/RecordsApi.js
+++ src/APIs/RecordsApi.js
@@ -28,7 +28,7 @@ export function getClassifications(parentId = "") {
 }
 
 export function getUser(userId) {
-    return fetch(serviceRoot + "users/"+userId);
+    return fetch(serviceRoot + "/users/"+userId);
 }
 
 export function deleteRecordById(recordId) {
