96bca5f0a6227772489a2c3ca6787dfeeac68575
Nicholas <nicholas.s.chan@gmail.com>
2018-03-18 17:50:10 -0700

    Persistance of user on refresh. Need to adjust for changes for user however

--- src/App.js
+++ src/App.js
@@ -49,34 +49,52 @@ class App extends Component {
     }
 
     componentWillMount(){
-        const search = window.location.search.substr(1);
-        const params = new URLSearchParams(search);
-        const userId = params.get('userId');
-        if (userId !== null) {
-            this.setState({ loading: true });
-            fetch(serviceRoot + "/users/"+userId)
-                .then(response => response.json())
-                .then(data => {
-                    this.setState({
-                        userData: data,
-                    });
-                    if(data.role === "Administrator" || data.role === "RMC") {
-                        this.setState({
-                            userAuthenticated: true,
-                        });
-                    }
-                    else {
-                        this.setState({
-                            userAuthenticated: false,
-                        });
-                    }
-                    setTimeout(() => {
-                        this.setState({loading: false}); }, 1500);
-                })
-                .catch(err => {
-                    console.error("Error loading record: " + err.message);
-                    this.setState({alertMsg: "The application was unable to connect to the server. Please try again later."})
+        let user = sessionStorage.getItem('user');
+        if (user !== null) {
+            let userJson =JSON.parse(user);
+            this.setState({userData: JSON.parse(user)});
+            if(userJson.role === "Administrator" || userJson.role === "RMC") {
+                this.setState({
+                    userAuthenticated: true,
                 });
+            }
+            else {
+                this.setState({
+                    userAuthenticated: false,
+                });
+            }
+        }
+        else {
+            const search = window.location.search.substr(1);
+            const params = new URLSearchParams(search);
+            const userId = params.get('userId');
+            if (userId !== null) {
+                this.setState({ loading: true });
+                fetch(serviceRoot + "/users/"+userId)
+                    .then(response => response.json())
+                    .then(data => {
+                        sessionStorage.setItem("user",JSON.stringify(data));
+                        this.setState({
+                            userData: data,
+                        });
+                        if(data.role === "Administrator" || data.role === "RMC") {
+                            this.setState({
+                                userAuthenticated: true,
+                            });
+                        }
+                        else {
+                            this.setState({
+                                userAuthenticated: false,
+                            });
+                        }
+                        setTimeout(() => {
+                            this.setState({loading: false}); }, 1500);
+                    })
+                    .catch(err => {
+                        console.error("Error loading record: " + err.message);
+                        this.setState({alertMsg: "The application was unable to connect to the server. Please try again later."})
+                    });
+            }
         }
     }
 
