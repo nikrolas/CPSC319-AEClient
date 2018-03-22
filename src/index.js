c873ba722e8d0c41cf04148b1a36cf2a9b272a8e
nicholas chan <nicholas.s.chan@gmail.com>
2018-03-22 16:52:03 -0700

    Included polyfill for URL Param for IE11

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'babel-polyfill'
+import 'url-search-params-polyfill';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css'
 import App from './App';
