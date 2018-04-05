4397f90b54f0401a06f654a227ea9ebaa85b9ed9
Ryan Koon <ryankoon@alumni.ubc.ca>
2018-04-05 05:48:09 -0700

    register service worker

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './App';
-import {unregister} from './registerServiceWorker';
+import register from './registerServiceWorker';
 import {
     BrowserRouter as Router,
 } from 'react-router-dom';
 
-unregister();
 ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
-
+register();
